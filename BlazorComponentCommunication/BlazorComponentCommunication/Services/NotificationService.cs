6bd0b8dd6819f65975d10b43012c0984ef05f766
Aathira <wpepele@gmail.com>
2023-08-03 22:46:32 +0200

    initial commit

---
+++ BlazorComponentCommunication/BlazorComponentCommunication/Services/NotificationService.cs
@@ -0,0 +1,21 @@
+﻿namespace BlazorComponentCommunication.Services
+{
+    public class NotificationService : INotificationService
+    {
+        public int Count { get; set; }
+
+        public event Action OnChange;
+
+        public void IncrementCounter()
+        {
+            Count++;
+            OnChange?.Invoke();
+        }
+
+        public void ResetCount()
+        {
+            Count = 0;
+            OnChange?.Invoke();
+        }
+    }
+}
